21770d7c904285c74d96cecdad706a264cd6ca9c
XNotBorisX <007boris05@gmail.com>
2026-05-22 01:49:26 +0300

    Add files via upload

---
+++ EventAccountingSystem/EventAccountingSystemAPI/Repositories/IEventRepository.cs
@@ -0,0 +1,12 @@
+﻿using EventAccountingSystemAPI.Models;
+
+namespace EventAccountingSystemAPI.Repositories;
+
+public interface IEventRepository
+{
+    Task<List<Event>> GetAllAsync();
+    Task<Event?> GetByIdAsync(Guid id);
+    Task AddAsync(Event eventItem);
+    Task UpdateAsync(Event eventItem);
+    Task DeleteAsync(Guid id);
+}
